50a08acf6c9a7f7db9589c00966603573102779b
PragmaticPhil <PragmaticPhil@users.noreply.github.com>
2017-11-25 00:36:13 +0000

    Add files via upload

---
+++ mbImageService_TetrisServer.py
@@ -0,0 +1,99 @@
+from microbit import*
+import radio
+radio.on()
+radio.config(length=50, queue=11, channel=11, power=6)
+
+global totalRows
+global totalCols
+totalRows = 10
+totalCols = 8
+
+
+shape_AtRest_Square_2x2 = [67, 34]
+shape_AtRest_Long_1x3 = [627]
+shape_AtRest_LeftUp_2x3 = [800, 314]
+shape_AtRest_RightUp_2x3 = [008, 314]
+shape_AtRest_MiddleUp_2x3 = [080, 314]
+
+shape_Square_2x2 = [11, 11]
+shape_Long_1x3 = [111]
+shape_LeftUp_2x3 = [100, 111]
+shape_RightUp_2x3 = [001, 111]
+shape_MiddleUp_2x3 = [010, 111]
+
+
+
+shapes_Rows = [2, 1, 2, 2, 2]                          #   [22, 13, 23, 23, 23]
+shapes_Cols = [2, 1, 3, 3, 3]
+
+shapes_defaultMasks = [ shape_Square_2x2, 
+                        shape_Long_1x3, 
+                        shape_LeftUp_2x3, 
+                        shape_RightUp_2x3, 
+                        shape_MiddleUp_2x3]
+
+
+global shapes_CurrentShapeType
+shapes_CurrentShapeType = 2
+
+global shapes_CurrentShapeLocation
+shapes_CurrentShapeLocation = [2, 3]
+
+global currentAnimationRow
+currentAnimationRow = 0
+
+
+def increaseAnimationRow():
+    global currentAnimationRow
+    currentAnimationRow = (currentAnimationRow + 1) % 5
+    
+
+def makeNextRowCommand(rowRef):
+    return ("tX120" + getPaddedRowRefStr(rowRef) + makeRowMask(rowRef))
+    
+def getPaddedRowRefStr(rowRef):
+    if (rowRef < 10):   return "0" + str(rowRef)
+    return str(rowRef)
+
+
+def makeRowMask(rowRef):
+    strBuf = "_"
+    for i in range(0, shapes_CurrentShapeLocation[1], 1):   strBuf  = strBuf  + "0"
+        
+    strBuf  = strBuf  + str(shapes_defaultMasks[shapes_CurrentShapeType][rowRef])
+    
+    remainingChars = totalCols - shapes_CurrentShapeLocation[1] - shapes_Cols[shapes_CurrentShapeType]
+    for i in range(0, remainingChars, 1):
+        strBuf  = strBuf  + "0"
+        
+    return strBuf
+
+def moveShapeHorizontally(moveDir):
+    global shapes_CurrentShapeLocation
+    shapes_CurrentShapeLocation[1] = shapes_CurrentShapeLocation[1] + moveDir
+    if(shapes_CurrentShapeLocation[1] <= 0):    shapes_CurrentShapeLocation[1] = 0
+    if((shapes_CurrentShapeLocation[1] + shapes_Cols[shapes_CurrentShapeType]) >= totalCols):   
+        shapes_CurrentShapeLocation[1] = (totalCols - shapes_Cols[shapes_CurrentShapeType])
+    print("--------------------------------------------------")
+    print("Direction = " + str(moveDir) +", width = " + str(shapes_Cols[shapes_CurrentShapeType]) + ", Pos = " + str(shapes_CurrentShapeLocation[1]))    
+
+
+def moveShapeDown():
+    global currentAnimationRow
+    global shapes_CurrentShapeLocation
+    currentAnimationRow = currentAnimationRow + 1
+    if(currentAnimationRow == 5):
+        currentAnimationRow = 0
+        shapes_CurrentShapeLocation[0] = shapes_CurrentShapeLocation[0] + 1
+
+
+
+while True:
+    print(makeNextRowCommand(1))
+    sleep(1000)
+    
+    if(button_a.was_pressed()):
+        moveShapeHorizontally(-1)
+
+    if(button_b.was_pressed()):
+        moveShapeHorizontally(1)    
---
+++ mbis_Tetris_Node.py
@@ -0,0 +1,81 @@
+from microbit import*
+import radio
+radio.on()
+radio.config(length=50, queue=11, channel=11, power=6)
+
+nodeID = 19
+
+global totalRows
+global totalCols
+totalRows = 1
+totalCols = nodeID + 1
+
+image_SpecialPreset_Full = "99999:99999:99999:99999:99999"
+image_SpecialPreset_Null = "00000:00000:00000:00000:00000"
+
+image_BorderPresets =       [   image_SpecialPreset_Null, 
+                                image_SpecialPreset_Full, 
+                                "55555:99999:99999:99999:99999", 
+                                "59999:59999:59999:59999:59999",
+                                "99995:99995:99995:99995:99995",
+                                "59995:59995:59995:59995:59995",
+                                "55555:59999:59999:59999:59999",
+                                "55555:99995:99995:99995:99995",
+                                "55555:59995:59995:59995:59995"
+                            ]
+
+image_AnimationPresets =    [   image_SpecialPreset_Full,
+                                "09999:09999:09999:09999:09999",
+                                "00999:00999:00999:00999:00999",
+                                "00099:00099:00099:00099:00099",
+                                "00009:00009:00009:00009:00009",
+                                image_SpecialPreset_Null
+                            ]
+
+global updateScreen
+updateScreen = False
+
+def getServerMessage():
+    try:        return str(radio.receive())
+    except:     return "X"
+
+
+def processServerMessage(rawMsg):
+    global updateScreen
+    
+    if(len(rawMsg) < 5):                return
+    if(not(rawMsg[0 : 2] == "tX")):     return
+
+    if(rawMsg[2 : 3] == "0"):                                               processServerInstruction(rawMsg)
+    if((rawMsg[2 : 3] == "1") and (int(rawMsg[5 : 7]) == getRowRef())):     processImageMessage(rawMsg)
+
+
+def processImageMessage(rawMsg):                                    #   tX12005100101 = tX12005  +  100101
+    rawMsgRef = 7 + getColRef()                                     #   take pos of node in row (col ref) and displaces it 7 chars to right
+    newDisplayImageRef = int(rawMsg[rawMsgRef : (rawMsgRef+ 1) ])   #   gets the bit that corresponds to the position of the node in this row   
+    display.show(Image(imagePresets[newDisplayImageRef]))
+ 
+def processServerInstruction(rawMsg):
+    instructionType = int(rawMsg[3 : 5])
+    #print(str(instructionType))
+    if(instructionType == 99): reset()
+    if(instructionType == 60): setMatrixDimensions(rawMsg[5 : 7], rawMsg[7 : 9]) 
+
+
+def setMatrixDimensions(rowStr, colStr):
+    global totalRows
+    global totalCols
+    totalRows = int(rowStr)
+    totalCols = int(colStr)
+
+def getColRef():
+    return(int(nodeID % totalCols))
+
+def getRowRef():
+    return int((nodeID - getColRef()) / totalCols)
+
+
+
+while True:
+    sleep(10)
+    processServerMessage(getServerMessage())
