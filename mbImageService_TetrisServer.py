76568e8fc8046358cc1e08814f12b710e402eefe
PragmaticPhil <PragmaticPhil@users.noreply.github.com>
2017-11-29 10:22:47 +0000

    Add files via upload

--- mbImageService_TetrisServer.py
+++ mbImageService_TetrisServer.py
@@ -1,66 +1,64 @@
 from microbit import*
 import radio
 radio.on()
-radio.config(length=50, queue=11, channel=11, power=6)
+radio.config(length=25, queue=20, channel=19, power=6)
 
 global totalRows
 global totalCols
 totalRows = 10
 totalCols = 8
 
+shape_Square = [22, 33, 55]
+shape_Long = [444, 555]
+shape_LeftUp = [255, 344, 555]
+shape_RightUp = [552, 443, 555]
+shape_MiddleUp = [525, 434, 555]
+shape_UpSquiggle = [255, 342, 554, 995]
 
-shape_AtRest_Square_2x2 = [67, 34]
-shape_AtRest_Long_1x3 = [627]
-shape_AtRest_LeftUp_2x3 = [800, 314]
-shape_AtRest_RightUp_2x3 = [008, 314]
-shape_AtRest_MiddleUp_2x3 = [080, 314]
+shapes_Rows = [3, 2, 3, 3, 3, 4]
+shapes_Cols = [2, 1, 3, 3, 3, 3]
 
-shape_Square_2x2 = [11, 11]
-shape_Long_1x3 = [111]
-shape_LeftUp_2x3 = [100, 111]
-shape_RightUp_2x3 = [001, 111]
-shape_MiddleUp_2x3 = [010, 111]
-
-
-
-shapes_Rows = [2, 1, 2, 2, 2]                          #   [22, 13, 23, 23, 23]
-shapes_Cols = [2, 1, 3, 3, 3]
-
-shapes_defaultMasks = [ shape_Square_2x2, 
-                        shape_Long_1x3, 
-                        shape_LeftUp_2x3, 
-                        shape_RightUp_2x3, 
-                        shape_MiddleUp_2x3]
-
+shapes_defaultMasks = [ shape_Square, 
+                        shape_Long, 
+                        shape_LeftUp, 
+                        shape_RightUp, 
+                        shape_MiddleUp, 
+                        shape_UpSquiggle]
 
 global shapes_CurrentShapeType
-shapes_CurrentShapeType = 2
+shapes_CurrentShapeType = 5
 
 global shapes_CurrentShapeLocation
-shapes_CurrentShapeLocation = [2, 3]
+shapes_CurrentShapeLocation = [2, 3]        #   2 = row, 3 = col
 
 global currentAnimationRow
 currentAnimationRow = 0
 
 
-def increaseAnimationRow():
-    global currentAnimationRow
-    currentAnimationRow = (currentAnimationRow + 1) % 5
-    
+def makeNextFrame():
+    #startTime = 
+    moveShapeDown()
+    # if animation frame = 0 clear the row above... build a generic clear row function and rem to parametise whether row is being 
+    # cleared cos block has shifted or cos of a tetris... in the case of the latter an animation is triggered in the node.
+    for i in range(0, shapes_Rows[shapes_CurrentShapeType], 1):
+        print(makeNextRowCommand(i))
+    print("----------------------------")
 
-def makeNextRowCommand(rowRef):
-    return ("tX120" + getPaddedRowRefStr(rowRef) + makeRowMask(rowRef))
+
+def makeNextRowCommand(currentShapeRow):
+    return ("tX120" + str(currentAnimationRow) + getPaddedRowRefStr(shapes_CurrentShapeLocation[0] + currentShapeRow) + getRowMask(currentShapeRow))
+
     
 def getPaddedRowRefStr(rowRef):
     if (rowRef < 10):   return "0" + str(rowRef)
     return str(rowRef)
 
 
-def makeRowMask(rowRef):
+def getRowMask(rowOfShape):        # each shape has several rows - we construct a new line for each row:
     strBuf = "_"
     for i in range(0, shapes_CurrentShapeLocation[1], 1):   strBuf  = strBuf  + "0"
         
-    strBuf  = strBuf  + str(shapes_defaultMasks[shapes_CurrentShapeType][rowRef])
+    strBuf  = strBuf  + str(shapes_defaultMasks[shapes_CurrentShapeType][rowOfShape])
     
     remainingChars = totalCols - shapes_CurrentShapeLocation[1] - shapes_Cols[shapes_CurrentShapeType]
     for i in range(0, remainingChars, 1):
@@ -68,14 +66,15 @@ def makeRowMask(rowRef):
         
     return strBuf
 
+
 def moveShapeHorizontally(moveDir):
     global shapes_CurrentShapeLocation
     shapes_CurrentShapeLocation[1] = shapes_CurrentShapeLocation[1] + moveDir
     if(shapes_CurrentShapeLocation[1] <= 0):    shapes_CurrentShapeLocation[1] = 0
     if((shapes_CurrentShapeLocation[1] + shapes_Cols[shapes_CurrentShapeType]) >= totalCols):   
         shapes_CurrentShapeLocation[1] = (totalCols - shapes_Cols[shapes_CurrentShapeType])
-    print("--------------------------------------------------")
-    print("Direction = " + str(moveDir) +", width = " + str(shapes_Cols[shapes_CurrentShapeType]) + ", Pos = " + str(shapes_CurrentShapeLocation[1]))    
+    #print("--------------------------------------------------")
+    #print("Direction = " + str(moveDir) +", width = " + str(shapes_Cols[shapes_CurrentShapeType]) + ", Pos = " + str(shapes_CurrentShapeLocation[1]))    
 
 
 def moveShapeDown():
@@ -84,12 +83,16 @@ def moveShapeDown():
     currentAnimationRow = currentAnimationRow + 1
     if(currentAnimationRow == 5):
         currentAnimationRow = 0
+        clearRow(shapes_CurrentShapeLocation[0])
         shapes_CurrentShapeLocation[0] = shapes_CurrentShapeLocation[0] + 1
 
+def clearRow(rowNum):
+    print("tX1210" + getPaddedRowRefStr(rowNum))
 
 
 while True:
-    print(makeNextRowCommand(1))
+        
+    makeNextFrame()
     sleep(1000)
     
     if(button_a.was_pressed()):
--- mbis_Tetris_Node.py
+++ mbis_Tetris_Node.py
@@ -1,14 +1,15 @@
 from microbit import*
 import radio
-radio.on()
-radio.config(length=50, queue=11, channel=11, power=6)
 
-nodeID = 19
+radio.on()
+radio.config(length=25, queue=20, channel=19, power=6)
+
+nodeID = 35
 
 global totalRows
 global totalCols
-totalRows = 1
-totalCols = nodeID + 1
+totalRows = 10
+totalCols = 10
 
 image_SpecialPreset_Full = "99999:99999:99999:99999:99999"
 image_SpecialPreset_Null = "00000:00000:00000:00000:00000"
@@ -24,13 +25,21 @@ image_BorderPresets =       [   image_SpecialPreset_Null,
                                 "55555:59995:59995:59995:59995"
                             ]
 
-image_AnimationPresets =    [   image_SpecialPreset_Full,
-                                "09999:09999:09999:09999:09999",
-                                "00999:00999:00999:00999:00999",
-                                "00099:00099:00099:00099:00099",
-                                "00009:00009:00009:00009:00009",
-                                image_SpecialPreset_Null
-                            ]
+image_AnimationPresets_FadeOut =    [   "00000:99999:99999:99999:99999",
+                                        "00000:00000:99999:99999:99999",
+                                        "00000:00000:00000:99999:99999",
+                                        "00000:00000:00000:00000:99999",
+                                        image_SpecialPreset_Null
+                                        ]
+
+
+image_AnimationPresets_FadeIn =     [   "99999:00000:00000:00000:00000",
+                                        "99999:99999:00000:00000:00000",
+                                        "99999:99999:99999:00000:00000",
+                                        "99999:99999:99999:99999:00000",
+                                        image_SpecialPreset_Full
+                                        ]
+
 
 global updateScreen
 updateScreen = False
@@ -46,18 +55,55 @@ def processServerMessage(rawMsg):
     if(len(rawMsg) < 5):                return
     if(not(rawMsg[0 : 2] == "tX")):     return
 
-    if(rawMsg[2 : 3] == "0"):                                               processServerInstruction(rawMsg)
-    if((rawMsg[2 : 3] == "1") and (int(rawMsg[5 : 7]) == getRowRef())):     processImageMessage(rawMsg)
+    print("Message is from server.  Target row = + " +rawMsg[6 : 8] + " - rowRef = " + str(getRowRef()))
+
+    if(rawMsg[2 : 3] == "0"):   processServerInstruction(rawMsg)
+    
+    if( (rawMsg[2 : 3] == "1") and (int(rawMsg[6 : 8]) == getRowRef()) ):
+            if(rawMsg[3 : 5] == "20"): processImageMessage(rawMsg)
+            if(rawMsg[3 : 5] == "21"): 
+                print("Clear screen")
+                display.clear()
+            if(rawMsg[3 : 5] == "22"): 
+                print("Destruction!")
+                runBlockDestroySequence()
 
 
 def processImageMessage(rawMsg):                                    #   tX12005100101 = tX12005  +  100101
     rawMsgRef = 7 + getColRef()                                     #   take pos of node in row (col ref) and displaces it 7 chars to right
-    newDisplayImageRef = int(rawMsg[rawMsgRef : (rawMsgRef+ 1) ])   #   gets the bit that corresponds to the position of the node in this row   
-    display.show(Image(imagePresets[newDisplayImageRef]))
+    blockType = int(rawMsg[rawMsgRef])
+    animationFrame = int(rawMsg[5])
+    print("Animation frame = " + str(animationFrame))
+
+    displayCorrectImage(blockType, animationFrame)
+
+    #newDisplayImageRef = int(rawMsg[rawMsgRef : (rawMsgRef+ 1) ])   #   gets the bit that corresponds to the position of the node in this row   
+    #display.show(Image(imagePresets[newDisplayImageRef]))
+ 
+def displayCorrectImage(blockType, animationFrame):
+    print("Block type = " + str(blockType))
+    if((blockType == 0) or (blockType == 9)):   
+        display.clear()
+        return
+
+    if((blockType == 1) or (blockType == 3)):
+        display.show(Image(image_SpecialPreset_Full))
+        return
+
+    if((blockType == 2) or (blockType == 4)):           # fading out...
+        display.show(Image(image_AnimationPresets_FadeOut[animationFrame]))
+        return
+
+    if(blockType == 5):                                 # fading in...
+        display.show(Image(image_AnimationPresets_FadeIn[animationFrame]))
+
+    return
+
+
  
 def processServerInstruction(rawMsg):
     instructionType = int(rawMsg[3 : 5])
-    #print(str(instructionType))
+    print(str(instructionType))
     if(instructionType == 99): reset()
     if(instructionType == 60): setMatrixDimensions(rawMsg[5 : 7], rawMsg[7 : 9]) 
 
@@ -74,8 +120,46 @@ def getColRef():
 def getRowRef():
     return int((nodeID - getColRef()) / totalCols)
 
+def runBlockDestroySequence():
+    display.show(Image("09990:99999:99999:99999:09990"))
+    sleep(50)
+    display.show(Image("00900:09990:99999:09990:00900"))
+    sleep(50)
+    display.show(Image("00900:00900:99999:00900:00900"))
+    sleep(50)
+    display.show(Image("00000:00900:09990:00900:00000"))
+    sleep(50)
+    display.show(Image("00000:00000:00900:00000:00000"))
+    sleep(50)
+    display.clear()
 
+
+def testBlockTypes():
+    #nodeID = 34         # Block Type 2
+    nodeID = 35         # Block Type 3
+    #nodeID = 36         # Block Type 4
+    #nodeID = 37         # Block Type 5
+    print("tX120003000255500")
+    processServerMessage("tX120003000234500")
+    sleep(1000)
+    print("tX120103000255500")
+    processServerMessage("tX120103000234500")
+    sleep(1000)
+    print("tX120203000255500")
+    processServerMessage("tX120203000234500")
+    sleep(1000)
+    print("tX120303000255500")
+    processServerMessage("tX120303000234500")
+    sleep(1000)    
+    print("tX120403000255500")
+    processServerMessage("tX120403000234500")
+    sleep(1000)    
+   
 
 while True:
     sleep(10)
-    processServerMessage(getServerMessage())
+    #processServerMessage(getServerMessage())
+    runBlockDestroySequence()
+    #testBlockTypes()
+    
+    
