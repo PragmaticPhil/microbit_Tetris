fa102eba3b5a77a0f48287691599547d957ca6a7
PragmaticPhil <PragmaticPhil@users.noreply.github.com>
2017-12-03 15:51:18 +0000

    Add files via upload

--- mbImageService_TetrisServer.py
+++ mbImageService_TetrisServer.py
@@ -53,7 +53,7 @@ def makeNewShape():
 def makeNextFrame():
     moveShapeDown()
     for i in range(0, shapes_Rows[shapes_CurrentShapeType], 1):
-        strBuf = ("tX120" + str(currentAnimationRow) + getPaddedRowRefStr(shapes_CurrentShapeLocation[0] + i) + getRowMask(i))
+        strBuf  = ("tX120" + str(currentAnimationRow) + getPaddedRowRefStr(shapes_CurrentShapeLocation[0] + i) + getRowMask(i))
         #radio.send("tX120" + str(currentAnimationRow) + getPaddedRowRefStr(shapes_CurrentShapeLocation[0] + i) + getRowMask(i))
 
     
@@ -64,14 +64,11 @@ def getPaddedRowRefStr(rowRef):
 
 def getRowMask(rowOfShape):        # each shape has several rows - we construct a new line for each row:
     strBuf = "_"
-    for i in range(0, shapes_CurrentShapeLocation[1], 1):   strBuf  = strBuf  + "0"
-        
-    strBuf  = strBuf  + str(shapes_defaultMasks[shapes_CurrentShapeType][rowOfShape])
-    
-    remainingChars = totalCols - shapes_CurrentShapeLocation[1] - shapes_Cols[shapes_CurrentShapeType]
-    for i in range(0, remainingChars, 1):
+    for i in range(shapes_CurrentShapeLocation[1]):
         strBuf  = strBuf  + "0"
-        
+    strBuf  = strBuf  + str(shapes_defaultMasks[shapes_CurrentShapeType][rowOfShape])
+    for i in range(totalCols - shapes_CurrentShapeLocation[1] - shapes_Cols[shapes_CurrentShapeType]):
+        strBuf  = strBuf  + "0"
     return strBuf
 
 
@@ -92,61 +89,58 @@ def moveShapeDown():
     if(currentAnimationRow == 5):
         currentAnimationRow = 0
         if(checkVerticalCollision()):
+            addShapeToBoard()
             printBoard()
             for i in range(shapes_CurrentShapeLocation[0], totalRows, 1):
-            #for i in range(shapes_CurrentShapeLocation[0], shapes_CurrentShapeLocation[0] + shapes_Rows[shapes_CurrentShapeType]-1, 1):                
-                checkForTetris(i)
+                if(checkForTetris(i)):  print("Tetris in row: " + str(i))
             makeNewShape()
         else:
             shapes_CurrentShapeLocation[0] = shapes_CurrentShapeLocation[0] + 1
 
-def clearRow(rowNum):
-    #print("tX1210" + getPaddedRowRefStr(rowNum))
-    for i in range(0, totalCols, 1):
-        setBoardBlockLocation(rowNum, i, 0)
-
-def setBoardBlockLocation(rowNum, colNum, val):
-    global board_blockLocations
-    insertPoint = (rowNum * totalCols) + colNum
-    #print("insert point = " + str(insertPoint))
-    board_blockLocations[insertPoint] = val
-
-
-def isBlockAt(rowNum, colNum):
-    rowMaskBuf = board_blockLocations[rowNum]
-    if(rowMaskBuf == 0): return False
-    return True
 
 def initialiseBlockArray(blockArray):
-    for i in range(0, (totalRows * totalCols), 1):
+    for i in range(totalRows * totalCols):
         blockArray.append(0)
 
 def checkForTetris(rowNum):
     global board_blockLocations
     for i in range(rowNum * totalCols, rowNum * totalCols + totalCols, 1):
-        if(board_blockLocations[i] == 0): return
+        if(board_blockLocations[i] == 0): return False
 
     print("tX1220" + getPaddedRowRefStr(rowNum))
     for j in range(rowNum, 1, -1):
         board_blockLocations[j * totalCols : j * totalCols + totalCols] = board_blockLocations[(j-1) * totalCols : (j-1) * totalCols + totalCols]
-
-
+    return True
+    
 def checkVerticalCollision():
-    if( (shapes_CurrentShapeLocation[0] + shapes_Rows[shapes_CurrentShapeType] - 1) >= totalRows):
-        addShapeToBoard()
-        return True
+    if( (shapes_CurrentShapeLocation[0] + shapes_Rows[shapes_CurrentShapeType] - 1) >= totalRows):        return True
+
+    for i in range(0, shapes_Rows[shapes_CurrentShapeType], 1):
+        for j in range(0, shapes_Cols[shapes_CurrentShapeType], 1):
+            if(getCharAt(getShapeRowStr(i), j) == "5"):
+                if(board_blockLocations[(i + shapes_CurrentShapeLocation[0]) * totalCols + shapes_CurrentShapeLocation[1] + j] == 1):
+                    #boardCharLocation = (i + shapes_CurrentShapeLocation[0]) * totalCols + shapes_CurrentShapeLocation[1] + j
+                    #boardCharStr = str(board_blockLocations[boardCharLocation])
+                    #print("B-col:" + getShapeRowStr(i) + "Location:" + str(j) + "Ar ref: " + str(boardCharLocation) + "... board val = " + boardCharStr)
+                    print("Buffer collision")
+                    return True
+
     return False
 
 
 def addShapeToBoard():  # shape has come to rest, now we need to add it to the Board Locations array:
     global board_blockLocations
     for i in range(0, shapes_Rows[shapes_CurrentShapeType], 1):
-        strBuf = str(shapes_defaultMasks[shapes_CurrentShapeType][i])
-
         for j in range(0, shapes_Cols[shapes_CurrentShapeType], 1):
-            if(not((strBuf[j] == "5") or (strBuf[j] == "9"))):
-                setBoardBlockLocation(shapes_CurrentShapeLocation[0] + i, shapes_CurrentShapeLocation[1] + j, 1)
+            curChar = getCharAt(getShapeRowStr(i), j)
+            if(not((curChar == "5") or (curChar == "9"))):
+                board_blockLocations[((shapes_CurrentShapeLocation[0] + i) * totalCols) + (shapes_CurrentShapeLocation[1] + j)] = 1
 
+def getShapeRowStr(rowNum):
+    return str(shapes_defaultMasks[shapes_CurrentShapeType][rowNum])
+
+def getCharAt(rowMask, colNum):
+    return (rowMask[colNum])
 
 def printBoard():
     for i in range(0, totalRows, 1):
